b0605bee22195bf07324cc3e691c20be593b79d9
afzalimamias <afzalimamias@gmail.com>
2023-11-18 15:04:07 +0530

    blogger api integration

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,42 @@
-import { useEffect } from "react";
-import { blogs } from "../constants";
+import { useEffect, useState } from "react";
+// import { blogs } from "../constants";
+import axios from "axios";
+
+const API_KEY = import.meta.env.VITE_BLOGGER_API_KEY;
+const BLOG_ID = import.meta.env.VITE_BLOG_ID;
 
 const Blog = () => {
+    const [pageToken, setPageToken] = useState("");
+    const [blogs, setBlogs] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    const fetchBlogs = async () => {
+        setLoading(true);
+        try {
+            const { data } = await axios.get(
+                `https://www.googleapis.com/blogger/v3/blogs/${BLOG_ID}/posts?key=${API_KEY}${
+                    pageToken ? `&pageToken=${pageToken}` : ""
+                }`
+            );
+            setBlogs(() => [...blogs, ...data.items]);
+            setPageToken(data.nextPageToken);
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setLoading(false);
+        }
+    };
+    console.log(blogs);
+
+    const handleMoreArticle = () => {
+        if (!pageToken) return;
+        fetchBlogs();
+    };
+
+    useEffect(() => {
+        fetchBlogs();
+    }, []);
+
     useEffect(() => {
         window.scrollTo(0, 0);
     }, []);
@@ -11,50 +46,59 @@ const Blog = () => {
                 <h2 className="h2 article-title">Blog</h2>
             </header>
 
-            <section className="blog-posts">
-                <ul className="blog-posts-list">
-                    {blogs.map((blog) => (
-                        <li key={blog.id} className="blog-post-item">
-                            <a href={blog.url} target="_blank">
-                                <figure className="blog-banner-box">
+            {!loading ? (
+                <section className="blog-posts">
+                    <ul className="blog-posts-list">
+                        {blogs.map((blog) => (
+                            <li key={blog.id} className="blog-post-item">
+                                <a href={blog.url} target="_blank">
+                                    {/* <figure className="blog-banner-box">
                                     <img
                                         src={blog.image}
                                         alt={blog.title}
                                         loading="lazy"
                                     />
-                                </figure>
+                                </figure> */}
 
-                                <div className="blog-content">
-                                    <div className="blog-meta">
-                                        <p className="blog-category">
-                                            {blog.category}
-                                        </p>
+                                    <div className="blog-content">
+                                        <div className="blog-meta">
+                                            {/* <p className="blog-category">
+                                            {blog.labels[0]}
+                                        </p> */}
 
-                                        <span className="dot"></span>
+                                            {/* <span className="dot"></span> */}
 
-                                        <time dateTime="2022-02-23">
-                                            {blog.date}
-                                        </time>
+                                            <time dateTime="2022-02-23">
+                                                {blog.updated
+                                                    .split("T")[0]
+                                                    .split("-")
+                                                    .reverse()
+                                                    .join("-")}
+                                            </time>
+                                        </div>
+
+                                        <h3 className="h3 blog-item-title">
+                                            {blog.title}
+                                        </h3>
+
+                                        <p className="blog-text">{blog.desc}</p>
                                     </div>
-
-                                    <h3 className="h3 blog-item-title">
-                                        {blog.title}
-                                    </h3>
-
-                                    <p className="blog-text">{blog.desc}</p>
-                                </div>
-                            </a>
-                        </li>
-                    ))}
-                </ul>
-                <a
-                    href="https://myeduwaves.com"
-                    target="_blank"
-                    className="blog-more-btn"
-                >
-                    More Articles
-                </a>
-            </section>
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                    <button
+                        onClick={handleMoreArticle}
+                        href="https://myeduwaves.com"
+                        target="_blank"
+                        className="blog-more-btn"
+                    >
+                        {!pageToken ? "That's All" : "Load More"}
+                    </button>
+                </section>
+            ) : (
+                <p className="loading-text">Loading...</p>
+            )}
         </article>
     );
 };
